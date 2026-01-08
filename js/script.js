8808d197ab8bdab79329ecca6d85d1ea38d0857e
Roberto Hernandez Cruz <240396@utxicotepec.edu.mx>
2026-01-08 14:29:45 -0600

    Crear funciones js y estilo del encabezdo

--- js/script.js
+++ js/script.js
@@ -1,3 +1,31 @@
 function saludar(){
     alert ("Bienvenido a la academia de T.I ");
 }
+
+function suma(){
+    var valor1=2;
+    var valor2=5;
+    var resultado = valor1 + valor2;
+    alert("Resultado de la suma es:"+resultado);
+}
+
+function resta(){
+    var valor1=2;
+    var valor2=5;
+    var resultado = valor1 - valor2;
+    alert("Resultado de la resta es:"+resultado);
+}
+
+function multiplicar(){
+    var valor1=2;
+    var valor2=5;
+    var resultado = valor1 * valor2;
+    alert("Resultado de la multiplicacion es:"+resultado);
+}
+
+function dividir(){
+    var valor1=2;
+    var valor2=5;
+    var resultado = valor1 / valor2;
+    alert("Resultado de la division es:"+resultado);
+}
